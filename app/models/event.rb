426e54c6fff8a119ba3c8a42e46cd3ea23c3d7b9
jiaowagn <wangjiaohere@gmail.com>
2018-05-18 11:34:45 +0800

    Admin can change events friendly_id

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,9 @@
 class Event < ApplicationRecord
   before_validation :generate_friendly_id, :on => :create
 
-  validates_presence_of :name
+  validates_presence_of :name, :friendly_id
+  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+  validates_uniqueness_of :friendly_id
 
 
   def to_param
