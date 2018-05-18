e17b02e9f0e460aa0fb8f29c24f164a41738d6c9
jiaowagn <wangjiaohere@gmail.com>
2018-05-18 15:17:37 +0800

    Add group & membership model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :memberhips
+  has_many :groups, :through => :memberhips
 
   def display_name
     self.email.split("@").first
