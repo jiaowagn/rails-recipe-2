b5341fd92ec1987e03bbfa569868ae839fea08e2
jiaowagn <wangjiaohere@gmail.com>
2018-05-18 11:25:47 +0800

    Add friendly_to events as params[:id]

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,16 @@
 class Event < ApplicationRecord
+  before_validation :generate_friendly_id, :on => :create
 
- validates_presence_of :name
+  validates_presence_of :name
 
- def to_param
-   "#{self.id}-#{self.name}"
- end
+
+  def to_param
+    self.friendly_id
+  end
+
+  protected
+    def generate_friendly_id
+      self.friendly_id ||= SecureRandom.uuid
+    end
 
 end
