9698e6a5e456116b2e8d2450ffc20719c95e644f
jiaowagn <wangjiaohere@gmail.com>
2018-05-19 22:03:11 +0800

    show error[:base] message with flash or directly on the form

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -6,6 +6,7 @@ class Registration < ApplicationRecord
 
   validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
   validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
+  validate :check_event_status, :on => :create
 
   belongs_to :event
   belongs_to :user, :optional => true
@@ -20,6 +21,12 @@ class Registration < ApplicationRecord
   protected
     def should_validate_basic_data?
       current_step == 2
+    end
+
+    def check_event_status
+      if self.event.status == "draft"
+        errors.add(:base, "活动尚未开放报名")
+      end
     end
 
     def should_validate_all_data?
