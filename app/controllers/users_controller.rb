0239a7f93d701f5e546f5e9e5402944bda6b7bf7
jiaowagn <wangjiaohere@gmail.com>
2018-05-19 10:50:05 +0800

    implement users#show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_user
+  def show
+  end
+
   def edit
-    @user = current_user
     @user.create_profile unless @user.profile
   end
 
   def update
-    @user = current_user
     if @user.update(user_params)
       flash[:notice] = "修改成功"
       redirect_to edit_user_path
@@ -16,6 +18,11 @@ class UsersController < ApplicationController
   end
 
   protected
+    def find_user
+      @user = current_user
+      @user.create_profile unless @user.profile 
+    end
+
     def user_params
       params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :education, :location, :occupation, :bio, :specialty, :birthday])
     end
