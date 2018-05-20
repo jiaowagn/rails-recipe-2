8903e01edc4126ee043535304247d6ab9de7b506
jiaowagn <wangjiaohere@gmail.com>
2018-05-20 16:27:52 +0800

    add date_field_tag for registration filter

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -16,7 +16,15 @@ class Admin::EventRegistrationsController < AdminController
 
     if Array(params[:ticket_ids]).any?
       @registrations = @registrations.by_ticket(params[:ticket_ids])
-    end 
+    end
+
+    if params[:start_on].present?
+      @registrations = @registrations.where("created_at >= ?", Date.parse(params[:start_on]).beginning_of_day)
+    end
+
+    if params[:end_on].present?
+      @registrations = @registrations.where("created_at <= ?", Date.parse(params[:end_on]).end_of_day)
+    end
   end
 
   def destroy
