ea4519710443f251b3cbdeed7e89257026bbcfbb
jiaowagn <wangjiaohere@gmail.com>
2018-05-20 15:21:31 +0800

    Filter registrations by status & ticket_id

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -14,6 +14,9 @@ class Registration < ApplicationRecord
 
   before_create :generate_uuid
 
+  scope :by_status, ->(s){where(:status => s)}
+  scope :by_ticket, ->(t){where(:ticket_id => t)}
+
   def to_param
     self.uuid
   end
