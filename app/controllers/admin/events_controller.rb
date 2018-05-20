41aff5fd75645ac9f403e1b5570e11824839777f
jiaowagn <wangjiaohere@gmail.com>
2018-05-20 19:49:09 +0800

    split Chart registrations/ticket by status

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -13,14 +13,18 @@ class Admin::EventsController < AdminController
               'rgba(153, 102, 255, 0.2)',
               'rgba(255, 159, 64, 0.2)']
     ticket_names = @event.tickets.map{|t| t.name}
+    status_colors = {"confirmed" => "#FF6384", "pending" => "#36A2EB"}
+
     @data1 = {
       labels: ticket_names,
-      datasets: [{
-        label: "# of Registrations",
-        data: @event.tickets.map{|t| t.registrations.count},
-        backgroundColor: colors,
-        borderWidth: 1
-        }]
+      datasets: Registration::STATUS.map do |s|
+        {
+          label: I18n.t(s, :scope => "registration.status"),
+          data: @event.tickets.map{|t| t.registrations.by_status(s).count},
+          backgroundColor: status_colors[s],
+          borderWidth: 1
+        }
+      end
     }
 
     @data2 = {
