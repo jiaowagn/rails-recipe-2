21b830048535b6b83bfbb41fa334d201bacf6074
jiaowagn <wangjiaohere@gmail.com>
2018-05-20 16:50:15 +0800

    Implement only_public/only_available for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,8 +12,11 @@ class Event < ApplicationRecord
 
   belongs_to :category, :optional => true
   has_many :tickets, :dependent => :destroy, :inverse_of => :event
-  has_many :registrations, :dependent => :destroy 
+  has_many :registrations, :dependent => :destroy
   accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
+
+  scope :only_public, -> {where(:status => "public")}
+  scope :only_available, -> {where(:status => ["private", "public"])}
 
   def to_param
     self.friendly_id
